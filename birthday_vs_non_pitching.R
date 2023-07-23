4e0b194d59fd0b4121b707a2e68b30a8b1cd87be
thomasemurphy <tommurphyemail@gmail.com>
2023-07-22 17:41:36 -0700

    did pitcher analysis

---
+++ birthday_vs_non_pitching.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+library(scales)
+
+library(Lahman)
+library(baseballr)
+
+setwd('baseball/birthdays')
+
+calculate_k_rate <- function(pitcher_df) {
+  tot_ip <- sum(pitcher_df$outs) / 3
+  tot_k <- sum(pitcher_df$strike_outs)
+  tot_k / tot_ip * 9
+}
+
+calculate_ra <- function(pitcher_df) {
+  tot_r <- sum(pitcher_df$runs)
+  tot_ip <- sum(pitcher_df$outs) / 3
+  tot_r / tot_ip * 9
+}
+
+birthday_pitchers <- read_csv('data/cleaned/birthday_pitcher_stats.csv') %>%
+  filter(!is.na(outs))
+
+birthday_pitchers$game_year <- substr(birthday_pitchers$game_date, 1, 4)
+unique_years <- unique(birthday_pitchers$game_year)
+
+birthday_boys_season_stats <- data.frame()
+for (year_str in unique_years) {
+  print(year_str)
+  season_stats_fname <- paste0('data/season_data/pitching_stats_',
+                               year_str,
+                               '.csv')
+  season_stats_df <- read_csv(season_stats_fname)
+  players_this_year <- birthday_pitchers %>%
+    filter(game_year == year_str) %>%
+    pull(player_id)
+  birthday_boys_this_year <- season_stats_df %>%
+    filter(player_id %in% players_this_year)
+  birthday_boys_season_stats <- rbind(
+    birthday_boys_season_stats,
+    birthday_boys_this_year
+  )
+}
+
+birthday_boys_season_stats %>%
+  select(player_id, season, player_full_name, runs, outs) %>%
+  print(n = 10)
+
+birthdays_stats <- c(
+  calculate_ra(birthday_pitchers),
+  calculate_k_rate(birthday_pitchers)
+)
+
+full_season_stats <- c(
+  calculate_ra(birthday_boys_season_stats),
+  calculate_k_rate(birthday_boys_season_stats)
+)
--- season_pitching_stats.R
+++ season_pitching_stats.R
@@ -8,7 +8,7 @@ library(baseballr)
 
 setwd('baseball/birthdays')
 
-# this script gets all the unique years in data/birthday_box_scores, and gets complete hitting stats for those years using baseballr::mlb_stats
+# this script gets all the unique years in data/birthday_box_scores, and gets complete pitching stats for those years using baseballr::mlb_stats
 
 data_files <- list.files(
   path = 'data/birthday_box_scores/',
@@ -20,13 +20,13 @@ data_files <- list.files(
 all_years <- lapply(list(as.numeric(unique(substr(data_files, 8, 11)))), sort)[[1]]
 
 for (year in all_years) {
-  hitting_stats <- mlb_stats(
+  pitching_stats <- mlb_stats(
     stat_type = 'season',
-    stat_group = 'hitting',
+    stat_group = 'pitching',
     player_pool = 'All',
     season = year,
     limit = 10000
   )
-  fname <- paste0('data/season_data/hitting_stats_', year, '.csv')
-  write_csv(hitting_stats, fname)
+  fname <- paste0('data/season_data/pitching_stats_', year, '.csv')
+  write_csv(pitching_stats, fname)
 }
